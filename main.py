2992b8997f38792f5f5be902aed7cd08d1abd2b3
Christopher Cox <christophercox@MacBook-Pro-5.fios-router.home>
2024-05-01 22:53:48 -0400

    :fire: Forgot to save

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2023-2024 Westfall Inc.
 #
-# This file is part of Windstorm-Dwarven.
+# This file is part of Windripper.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
